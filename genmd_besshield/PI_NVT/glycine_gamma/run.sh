b4193f154e422239314f3888db0beb784b078db5
Victor Principe <victorprincipe1206@gmail.com>
2022-02-20 20:41:28 +0100

    Added some sim files and results.

--- genmd_besshield/PI_NVT/glycine_gamma/run.sh
+++ genmd_besshield/PI_NVT/glycine_gamma/run.sh
@@ -2,9 +2,8 @@
 
 # initialize the socket and set up the simulation
 ~/i-pi/bin/i-pi input.xml > log.ipi &
-sleep 10
+sleep 30
 ~/i-pi/bin/i-pi-py_driver -u -a gapd-nvt-gg -m rascal -o ../../CSD_GAP_model.json,init.extxyz > log.gap &
-
 ~/dftb+/bin/dftb+ > log.dftb &
 wait
 
