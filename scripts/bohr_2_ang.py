97c72cd201e0353df9052054b973a38451237755
Victor Principe <victorprincipe1206@gmail.com>
2022-02-21 17:56:25 +0100

    Added script for converting ase-formatted xyz trajectory files from bohr to angstroms.

---
+++ scripts/bohr_2_ang.py
@@ -0,0 +1,22 @@
+import sys
+import ase
+from ase.io import read, write
+
+'''
+Usage: python bohr_2_ang.py (xyz file)
+Convert an ASE-formatted xyz trajectory from bohr to angstroms. This is required for Matthias's general shiedling model.
+New file is saved as (old_filename)_ang.xyz
+'''
+
+def bohr_2_ang(file):
+    bohr = read(file, index=":") # Read file
+
+    for frame in bohr:
+        frame.cell *= ase.units.Bohr  # Convert cell params to Bohr
+        frame.positions *= ase.units.Bohr  # Convert positions to Bohr
+
+    write(file[:-4]+'_ang.xyz', bohr) # Save file 
+    print("Conversion complete. File saved as: "+file[:-4]+'_ang.xyz' )
+
+bohr_2_ang(sys.argv[1])
+
