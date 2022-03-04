92c5a7d643d333dd2094e148d4132fa2201ef429
Victor Principe <victorprincipe1206@gmail.com>
2022-03-04 16:47:47 +0100

    Added GenMD + BesShield files and results.

--- genmd_besshield/PI_NVT/benzene_I/job.sh
+++ genmd_besshield/PI_NVT/benzene_I/job.sh
@@ -3,15 +3,20 @@
 #SBATCH --time=48:00:00
 #SBATCH --partition=jobs
 #SBATCH --nodes=1                    # Run all processes on a single node
-#SBATCH --ntasks=64
+#SBATCH --ntasks-per-node=64
 
+module purge
+module load intel
+module load intel-mkl
+module load intel-mpi
 
-rm /tmp/ipi_dftb_nvt /tmp/ipi_gap_d_nvt
+rm /tmp/ipi_dftb-nvt-bI /tmp/ipi_gapd-nvt-bI
 
 i-pi input.xml > log.ipi &
-sleep 30
+sleep 10
 
-mpirun -np 32 i-pi-py_driver -u -a gapd-nvt-bI -m rascal -o ../../CSD_GAP_model.json,init.extxyz > log.gap &
+mpirun -np 31 i-pi-py_driver -u -a gapd-nvt-bI -m rascal -o ../../CSD_GAP_model.json,init.extxyz > log.gap &
 
-mpirun -np 32 ~/code/dftb+/bin/dftb+ > log.dftb &
+mpirun -np 31 ~/code/dftb+/bin/dftb+ > log.dftb &
 wait
+exit 0
